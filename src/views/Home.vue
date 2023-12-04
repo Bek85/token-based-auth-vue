811320acbf49d504c8e9922f1ab0ebbda05bf4c5
Bekzod Akbarov <stx.webdev@gmail.com>
2023-12-04 19:22:06 +0500

    create login and register router links

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="home">
     <h1>Welcome to the App!</h1>
+    <div>
+      To use this app you'll need to
+      <router-link to="/login">Login</router-link>
+      or
+      <router-link to="/register">Register</router-link>
+    </div>
   </div>
 </template>
 
